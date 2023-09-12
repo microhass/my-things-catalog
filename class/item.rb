df78c145d7d61d7bb71e759f465a1bc532ac8c6c
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-09-12 10:09:17 +0200

    Create item methods

--- class/item.rb
+++ class/item.rb
@@ -1,14 +1,25 @@
-Class Item
-attr_accessor :archived, :publish_date
+class Item
+  attr_accessor :archived, :publish_date
 
-def initialize(archived, publish_date)
-  @id = generate_unique_id
-  @archived = archived
-  @publish_date = publish_date
-end
+  def initialize(archived, publish_date)
+    @id = generate_unique_id
+    @archived = archived
+    @publish_date = publish_date
+  end
 
-def generate_unique_id
-  timestamp = Time.now.to_i * 1000
-  random_number = rand(10_000)
-  "#{timestamp}#{random_number}"
+  def generate_unique_id
+    timestamp = Time.now.to_i * 1000
+    random_number = rand(10_000)
+    "#{timestamp}#{random_number}"
+  end
+
+  def move_to_archive
+      p "moved"
+  end
+  
+  private
+
+  def can_be_archived?
+    p "archive"
+  end
 end
