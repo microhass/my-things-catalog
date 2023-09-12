b2ca48d3f98d839e2232f93abd23c2859cc77851
Anand Mohan Pathak <anandpathak8317@gmail.com>
2023-09-12 22:12:18 +0530

    Add properties of game and author class

--- classes/author.rb
+++ classes/author.rb
@@ -1,5 +1,20 @@
 class Author
-  def initialize
-    'Hello Author'
+  def initialize(first_name, last_name)
+    @id = generate_unique_id
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def add_item(item)
+    @items << item
+    item.author = self
+  end
+
+  private
+
+  def generate_unique_id
+    timestamp = Time.now.to_i * 1000
+    random_number = rand(10_000)
+    "Aut#{timestamp}#{random_number}"
   end
 end
--- classes/game.rb
+++ classes/game.rb
@@ -1,5 +1,15 @@
-class Game
-  def initialize
-    'Hello Game'
+require_relative 'item'
+
+class Game < Item
+  def initialize(multilayer, last_played_at)
+    super
+    @multilayer = multilayer
+    @last_played_at = last_played_at
+  end
+
+  private
+
+  def can_be_archived?
+    super and (Time.new.year - @last_played_at.year) > 2
   end
 end
