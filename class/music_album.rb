e4a9ae2b8ff6a2efdc49dc3b0f2d30bf02972690
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-09-13 20:45:13 +0200

    Define class properties and constuctor method

--- class/music_album.rb
+++ class/music_album.rb
@@ -1,3 +1,10 @@
-class MusicAlbum
-    p "music album class"
+require_relative 'item'
+
+class MusicAlbum < Item
+    attr_accessor :on_spotify
+
+    def initialize(genre, author, source, labor, publish_date)
+        super(genre, author, source, labor, publish_date)
+        @on_spotify: on_spotify
+    end
 end
