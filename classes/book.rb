3be38ccb7aae0cfe343678902c2d9f8558ece823
hassanShakur <hass.shakurhassan@gmail.com>
2023-09-13 15:15:18 +0300

    add unit tests for book class methods

--- classes/book.rb
+++ classes/book.rb
@@ -3,8 +3,8 @@ require_relative 'item'
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(archived, publish_date, publisher, cover_state)
-    super(archived, publish_date)
+  def initialize(publisher, cover_state, publish_date)
+    super(false, publish_date)
     @publisher = publisher
     @cover_state = cover_state
   end
