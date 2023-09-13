30209ec19423e84bfcc1a761d6130fa37a5a212e
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-09-13 15:35:08 +0200

    Modify item class

--- class/item.rb
+++ class/item.rb
@@ -1,10 +1,14 @@
 class Item
-  attr_accessor :archived, :publish_date
+  attr_accessor :archived, :publish_date, :genre, :author, :source, :label
 
   def initialize(archived, publish_date)
     @id = generate_unique_id
     @archived = archived
     @publish_date = publish_date
+    @genre = nil
+    @author = nil
+    @source = nil
+    @label = nil
   end
 
   def generate_unique_id
@@ -14,12 +18,12 @@ class Item
   end
 
   def move_to_archive
-      p "moved"
+    @archived = can_be_archived?
   end
-  
+
   private
 
   def can_be_archived?
-    p "archive"
+    (Time.new.year - @publish_date.year) > 10
   end
 end
