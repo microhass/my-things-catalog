818c5173a435f331f3a488996542fe1c187e86f0
Anand Mohan Pathak <anandpathak8317@gmail.com>
2023-09-14 11:51:32 +0530

    Data preserved

--- modules/items_display.rb
+++ modules/items_display.rb
@@ -33,7 +33,7 @@ module ItemsDisplay
     s_no = 1
     ObjectSpace.each_object(Game) do |obj|
       print "Serial Number: #{s_no}, Multiplayer: #{obj.multiplayer} Last played at: #{obj.last_played_at} "
-      puts "Author: #{obj.author.first_name} #{obj.author.last_name}"
+      puts "Archived: #{obj.archived} Publish date: #{obj.publish_date}"
       s_no += 1
     end
   end
