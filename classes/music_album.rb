e868f05cf810b78d26a2522dc0a13d2512dab668
Anand Mohan Pathak <anandpathak8317@gmail.com>
2023-09-14 13:25:21 +0530

    Update music_album.rb

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -3,7 +3,7 @@ require_relative 'item'
 class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initialize(so_spotify, archived, publish_date)
+  def initialize(on_spotify, archived, publish_date)
     super(archived, publish_date)
     @on_spotify = on_spotify
   end
